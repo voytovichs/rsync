ccf5107b1c9116701704c1d1bf929b442c604619
Sergey Voytovich <voytovich.sergeey@gmail.com>
2017-02-18 16:24:22 +0300

    Add required for building files

---
+++ cmake-build-debug/CMakeFiles/rsync.dir/DependInfo.cmake
@@ -0,0 +1,94 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/voytovichs/Code/rsync/access.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/access.c.o"
+  "/Users/voytovichs/Code/rsync/acls.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/acls.c.o"
+  "/Users/voytovichs/Code/rsync/authenticate.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/authenticate.c.o"
+  "/Users/voytovichs/Code/rsync/backup.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/backup.c.o"
+  "/Users/voytovichs/Code/rsync/batch.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/batch.c.o"
+  "/Users/voytovichs/Code/rsync/checksum.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/checksum.c.o"
+  "/Users/voytovichs/Code/rsync/chmod.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/chmod.c.o"
+  "/Users/voytovichs/Code/rsync/cleanup.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/cleanup.c.o"
+  "/Users/voytovichs/Code/rsync/clientname.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/clientname.c.o"
+  "/Users/voytovichs/Code/rsync/clientserver.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/clientserver.c.o"
+  "/Users/voytovichs/Code/rsync/compat.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/compat.c.o"
+  "/Users/voytovichs/Code/rsync/connection.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/connection.c.o"
+  "/Users/voytovichs/Code/rsync/delete.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/delete.c.o"
+  "/Users/voytovichs/Code/rsync/exclude.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/exclude.c.o"
+  "/Users/voytovichs/Code/rsync/fileio.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/fileio.c.o"
+  "/Users/voytovichs/Code/rsync/flist.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/flist.c.o"
+  "/Users/voytovichs/Code/rsync/generator.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/generator.c.o"
+  "/Users/voytovichs/Code/rsync/getfsdev.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/getfsdev.c.o"
+  "/Users/voytovichs/Code/rsync/getgroups.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/getgroups.c.o"
+  "/Users/voytovichs/Code/rsync/hashtable.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/hashtable.c.o"
+  "/Users/voytovichs/Code/rsync/hlink.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/hlink.c.o"
+  "/Users/voytovichs/Code/rsync/io.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/io.c.o"
+  "/Users/voytovichs/Code/rsync/lib/compat.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/lib/compat.c.o"
+  "/Users/voytovichs/Code/rsync/lib/getaddrinfo.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/lib/getaddrinfo.c.o"
+  "/Users/voytovichs/Code/rsync/lib/getpass.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/lib/getpass.c.o"
+  "/Users/voytovichs/Code/rsync/lib/inet_ntop.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/lib/inet_ntop.c.o"
+  "/Users/voytovichs/Code/rsync/lib/inet_pton.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/lib/inet_pton.c.o"
+  "/Users/voytovichs/Code/rsync/lib/md5.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/lib/md5.c.o"
+  "/Users/voytovichs/Code/rsync/lib/mdfour.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/lib/mdfour.c.o"
+  "/Users/voytovichs/Code/rsync/lib/permstring.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/lib/permstring.c.o"
+  "/Users/voytovichs/Code/rsync/lib/pool_alloc.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/lib/pool_alloc.c.o"
+  "/Users/voytovichs/Code/rsync/lib/snprintf.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/lib/snprintf.c.o"
+  "/Users/voytovichs/Code/rsync/lib/sysacls.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/lib/sysacls.c.o"
+  "/Users/voytovichs/Code/rsync/lib/sysxattrs.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/lib/sysxattrs.c.o"
+  "/Users/voytovichs/Code/rsync/lib/wildmatch.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/lib/wildmatch.c.o"
+  "/Users/voytovichs/Code/rsync/loadparm.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/loadparm.c.o"
+  "/Users/voytovichs/Code/rsync/log.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/log.c.o"
+  "/Users/voytovichs/Code/rsync/main.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/main.c.o"
+  "/Users/voytovichs/Code/rsync/match.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/match.c.o"
+  "/Users/voytovichs/Code/rsync/options.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/options.c.o"
+  "/Users/voytovichs/Code/rsync/params.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/params.c.o"
+  "/Users/voytovichs/Code/rsync/pipe.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/pipe.c.o"
+  "/Users/voytovichs/Code/rsync/popt/findme.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/popt/findme.c.o"
+  "/Users/voytovichs/Code/rsync/popt/popt.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/popt/popt.c.o"
+  "/Users/voytovichs/Code/rsync/popt/poptconfig.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/popt/poptconfig.c.o"
+  "/Users/voytovichs/Code/rsync/popt/popthelp.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/popt/popthelp.c.o"
+  "/Users/voytovichs/Code/rsync/popt/poptparse.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/popt/poptparse.c.o"
+  "/Users/voytovichs/Code/rsync/progress.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/progress.c.o"
+  "/Users/voytovichs/Code/rsync/receiver.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/receiver.c.o"
+  "/Users/voytovichs/Code/rsync/rounding.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/rounding.c.o"
+  "/Users/voytovichs/Code/rsync/rsync.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/rsync.c.o"
+  "/Users/voytovichs/Code/rsync/sender.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/sender.c.o"
+  "/Users/voytovichs/Code/rsync/socket.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/socket.c.o"
+  "/Users/voytovichs/Code/rsync/support/savetransfer.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/support/savetransfer.c.o"
+  "/Users/voytovichs/Code/rsync/syscall.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/syscall.c.o"
+  "/Users/voytovichs/Code/rsync/t_stub.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/t_stub.c.o"
+  "/Users/voytovichs/Code/rsync/t_unsafe.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/t_unsafe.c.o"
+  "/Users/voytovichs/Code/rsync/testrun.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/testrun.c.o"
+  "/Users/voytovichs/Code/rsync/tls.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/tls.c.o"
+  "/Users/voytovichs/Code/rsync/token.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/token.c.o"
+  "/Users/voytovichs/Code/rsync/trimslash.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/trimslash.c.o"
+  "/Users/voytovichs/Code/rsync/uidlist.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/uidlist.c.o"
+  "/Users/voytovichs/Code/rsync/util.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/util.c.o"
+  "/Users/voytovichs/Code/rsync/util2.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/util2.c.o"
+  "/Users/voytovichs/Code/rsync/wildtest.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/wildtest.c.o"
+  "/Users/voytovichs/Code/rsync/xattrs.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/xattrs.c.o"
+  "/Users/voytovichs/Code/rsync/zlib/adler32.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/zlib/adler32.c.o"
+  "/Users/voytovichs/Code/rsync/zlib/compress.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/zlib/compress.c.o"
+  "/Users/voytovichs/Code/rsync/zlib/crc32.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/zlib/crc32.c.o"
+  "/Users/voytovichs/Code/rsync/zlib/deflate.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/zlib/deflate.c.o"
+  "/Users/voytovichs/Code/rsync/zlib/inffast.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/zlib/inffast.c.o"
+  "/Users/voytovichs/Code/rsync/zlib/inflate.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/zlib/inflate.c.o"
+  "/Users/voytovichs/Code/rsync/zlib/inftrees.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/zlib/inftrees.c.o"
+  "/Users/voytovichs/Code/rsync/zlib/trees.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/zlib/trees.c.o"
+  "/Users/voytovichs/Code/rsync/zlib/zutil.c" "/Users/voytovichs/Code/rsync/cmake-build-debug/CMakeFiles/rsync.dir/zlib/zutil.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
